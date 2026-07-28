b5c6e13993f346e416250d18d20c0ed36d1cb725
Shah-zaibAly <shahxaib269@gmail.com>
2026-07-28 14:57:25 +0500

    Excercise - 6th

--- 6th.py
+++ 6th.py
@@ -1,2 +1,11 @@
 import pandas as pd
 
+# Excercise - Searching a Pokeamon by a name.
+
+df = pd.read_csv('Pokeamon.csv', index_col="Name")
+Pokeamon = input("Enter a Pokeamon name: ")
+
+try:
+    print(df.loc[Pokeamon])
+except KeyError:
+    print(f"{Pokeamon} not found.")
